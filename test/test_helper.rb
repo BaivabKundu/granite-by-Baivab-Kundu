5d133d376ba5556f1b813d98605d6befe481d733
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-03-13 11:32:18 +0530

    Added controller tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,6 +14,15 @@ def enable_test_coverage
   end
 end
 
+def headers(user, options = {})
+  {
+    Accept: "application/json",
+    "Content_Type" => "application/json",
+    "X-Auth-Token" => user.authentication_token,
+    "X-Auth-Email" => user.email
+  }.merge(options)
+end
+
 enable_test_coverage if ENV["COVERAGE"]
 
 ENV["RAILS_ENV"] ||= "test"
@@ -28,7 +37,7 @@ module ActiveSupport
     parallelize(workers: :number_of_processors) unless ENV["COVERAGE"]
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+    # fixtures :all
 
     # Add more helper methods to be used by all tests here...
   end
